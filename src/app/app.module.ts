d80dda4bc3a4cd4154cf402cc0c8587760f7f294
Daniel Sitali <danielmsitali@outlook.com>
2018-09-07 07:01:12 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './client/home/home.component';
+import { HeaderComponent } from './client/header/header.component';
+import { FooterComponent } from './client/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
